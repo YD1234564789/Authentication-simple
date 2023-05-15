0a7d2c11141536806df4815f6ed60ea47883a388
YD1234564789 <a8111202003@gmail.com>
2023-05-15 19:34:38 +0800

    feat: add index.hbs

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+// const Users = require('')
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  return Users.findById(email)
 })
 
 app.listen(port, () =>{
