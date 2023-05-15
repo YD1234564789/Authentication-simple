e05481114dc304a240ecaa133b78a9e6804b98f8
YD1234564789 <a8111202003@gmail.com>
2023-05-15 20:49:20 +0800

    feat: add authentication function

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const users = require('./models/seeds/users')
 const app = express()
 const port = 3000
-// const Users = require('')
+
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended:false}))
 
+// 首頁
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// 驗證資料
 app.post('/login', (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  return Users.findById(email)
+  const { email, password } = req.body
+  const user = users.find(user => user.email === email)
+  let errorMeg = ''
+
+  if (!user) {
+    errorMeg = 'Email error'
+    res.render('index', { errorMeg })
+    return
+  }
+  if (user.password !== password) {
+    errorMeg = 'Password error'
+    res.render('index', { errorMeg })
+    return
+  }
+  res.redirect(`/welcome/${user.firstName}`)
 })
+
+// 登入成功
+app.get('/welcome/:name', (req, res) => {
+  const name = req.params.name
+  res.render('welcome', { name })
+})
+
 
 app.listen(port, () =>{
   console.log(`App is running on ${port}`)
